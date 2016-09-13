1cb768491cef4a18df1a1745c041f3c0a6fb9d0e
JHOWELL <atlas3141@gmail.com>
2016-09-13 10:29:33 -0700

    Command line interface

--- StudentList.cpp
+++ StudentList.cpp
@@ -9,4 +9,33 @@ struct Student{
   char name;
   int id;
   float gpa;
+};
+
+int main(){
+  vector<Student*> list;
+  char input[20];
+  bool running = true;
+  while (running == true){
+    cin.get(input,20);
+    cin.ignore();
+    if (!strcmp(input,"PRINT")){
+      
+    }
+    else if (!strcmp(input, "ADD")){
+
+    }
+    else if (!strcmp(input, "DELETE")){
+
+    }
+    else if (!strcmp(input, "HELP")){
+      cout << "Commands: \nPRINT\nADD\nDELETE" << endl;
+    }
+    else if (!strcmp(input, "EXIT")){
+      running = false;
+    }
+    else{
+      cout << "No such command: " <<  input << endl;
+      cout << "Type HELP for a list of commands" << endl;
+    }
+  }
 }
