7cdabe1f76ac8207e0c98a80fef44a28e2d68e7c
JHOWELL <atlas3141@gmail.com>
2016-09-16 12:04:01 -0700

    Delete and Add Method

--- StudentList.cpp
+++ StudentList.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 struct Student{
-  char name;
+  char* name;
   int id;
   float gpa;
 };
@@ -49,8 +49,41 @@ void printStudents(vector<Student*>* list){
   }
 }
 void newStudent(vector<Student*>* list){
-
+  Student* s = new Student;
+  int newId;
+  char newName[30];
+  float newGpa;
+  cout << "New ID?" << endl;
+  cin >> newId;
+  cin.ignore();
+  cout << "New Name?" << endl;
+  cin >> newName;
+  cin.ignore();
+  cout << "New GPA?" << endl;
+  cin >> newGpa;
+  cin.ignore();
+  s->id = newId;
+  s->gpa = newGpa;
+  s->name = strdup(newName);
+  list->push_back(s);
 }
 void deleteStudent(vector<Student*>* list){
-
+  bool deleted = true;
+  int deleteId;
+  cin >> deleteId;
+  cin.ignore();
+  for(vector<Student*>::iterator it = list->begin(); it != list-> end(); it++){
+    if((*it)->id == deleteId){
+      cout << "Removed " << (*it)->name << endl;
+      delete (*it)->name;
+      delete *it;
+      list->erase(it);
+      it = list->end();
+      it--;
+      deleted = true;
+    }
+  }
+  if (deleted == true){
+    cout << "No entries wit Id " << deleteId << endl;
+  }
 } 
